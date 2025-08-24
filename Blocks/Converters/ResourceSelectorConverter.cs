a4cfc208efb2314e0ad354357b93c856e3c0103e
mimoguz <mimoguz@users.noreply.github.com>
2025-08-24 22:56:02 +0300

    Symbolic icon experiment

---
+++ Blocks/Converters/ResourceSelectorConverter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Styling;
+
+namespace Blocks.Converters;
+
+public class ResourceSelectorConverter : ResourceDictionary, IValueConverter
+{
+    public object? Convert(object? key, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var themeKey = $"{Application.Current?.ActualThemeVariant ?? ThemeVariant.Light}_{key!}";
+        Console.WriteLine(themeKey);
+        TryGetResource(themeKey, null, out var value);
+        return value;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
